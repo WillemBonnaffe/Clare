d5e885952602c51470a3e30523d782415eae555d
WillemBonnaffe <64412348+WillemBonnaffe@users.noreply.github.com>
2021-06-16 16:54:53 +0100

    Updated individual drug database

--- docs/database/splitPredictions.r
+++ docs/database/splitPredictions.r
@@ -5,9 +5,10 @@
 predictions = read.table("predictions.csv",sep=";",header=F)
 
 ## circulate through each unique drug code
-BNFVector = unique(predictions[,1])[1:2]
+BNFVector = unique(predictions[,1])[1:100]
 for(BNF in BNFVector)
 {
 	predictions_ = predictions[predictions[,1]==BNF,]
+	colnames(predictions_) = c("id","time","Y","Ybar","Ybar_lo","Ybar_hi")
 	write.table(predictions_,paste("individual_drugs/",BNF,".csv",sep=""),sep=";",row.names=FALSE,quote=FALSE)
 }
